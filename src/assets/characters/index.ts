c24a97121d76dc2fcdcf486fff10232106c4bd85
Brainzilla Studios <samvanderpal@gmail.com>
2026-03-21 15:46:25 +0000

    Update index.ts

--- src/assets/characters/index.ts
+++ src/assets/characters/index.ts
@@ -3,23 +3,19 @@ import { AvatarData } from '../../types';
 import barrattNeutral from './mobile/Barratt/barrat_neutral.png';
 import barrattHappy from './mobile/Barratt/barratt_happy.png';
 import barrattThink from './mobile/Barratt/barratt_think.png';
-import barrattAnimated from './barrattanimated.mp4';
 
 import branNeutral from './mobile/Bran/bran_neutral.png';
 import branHappy from './mobile/Bran/bran_happy.png';
 import branThink from './mobile/Bran/bran_think.png';
 import branAttack from './mobile/Bran/bran_attack.png';
-import branAnimated from './brananimated.mp4';
 
 import mochiNeutral from './mobile/Mochi/mochi_neutral.png';
 import mochiHappy from './mobile/Mochi/mochi_happy.png';
 import mochiSmile from './mobile/Mochi/mochi_smile.png';
-import mochiAnimated from './mochianimated.mp4';
 
 import vexNeutral from './mobile/Vex/vex_neutral.png';
 import vexSmile from './mobile/Vex/vex_smile.png';
 import vexThink from './mobile/Vex/vex_think.png';
-import vexAnimated from './vexanimated.mp4';
 
 export const DEFAULT_AVATAR_ID = 'barratt';
 
@@ -29,7 +25,6 @@ export const CHARACTER_AVATARS: AvatarData[] = [
     name: 'Barratt',
     image: barrattNeutral,
     portrait: barrattNeutral,
-    portraitVideo: barrattAnimated,
     color: 'bg-emerald-100',
     rarity: 'Common',
     level: 1,
@@ -44,7 +39,6 @@ export const CHARACTER_AVATARS: AvatarData[] = [
     name: 'Bran',
     image: branNeutral,
     portrait: branNeutral,
-    portraitVideo: branAnimated,
     color: 'bg-sky-100',
     rarity: 'Rare',
     level: 4,
@@ -61,7 +55,6 @@ export const CHARACTER_AVATARS: AvatarData[] = [
     name: 'Mochi',
     image: mochiNeutral,
     portrait: mochiNeutral,
-    portraitVideo: mochiAnimated,
     color: 'bg-rose-100',
     rarity: 'Epic',
     level: 7,
@@ -76,7 +69,6 @@ export const CHARACTER_AVATARS: AvatarData[] = [
     name: 'Vex',
     image: vexNeutral,
     portrait: vexNeutral,
-    portraitVideo: vexAnimated,
     color: 'bg-sky-100',
     rarity: 'Legendary',
     level: 10,
